f133c0b5304b7db11f966fca862bb680e954af34
Kyrylo Shyvam Kumar <kirilkumar@tutanota.com>
2023-10-28 14:50:18 +0530

    Commit-2.

---
+++ code/batch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -A kyrylo
+#SBATCH -n 9
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=2048
+#SBATCH --time=0-04:00:00
+#SBATCH --mail-type=END
+#SBATCH -w gnode084
+
+
+source ~/cpu1/bin/activate
+
+which python3
+echo I started
+./scripts/train.sh
+
