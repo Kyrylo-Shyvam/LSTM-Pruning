5ad468112aeebb2005c3babb5bd9085fec8f60a6
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-11-02 02:00:26 +0530

    retraining

--- code/lstmModel.py
+++ code/lstmModel.py
@@ -8,6 +8,7 @@ A very basic implementation of neural machine translation
 
 Usage:
     nmt.py train --train-src=<file> --train-tgt=<file> --dev-src=<file> --dev-tgt=<file> --vocab=<file> [options]
+    nmt.py retrain --train-src=<file> --train-tgt=<file> --dev-src=<file> --dev-tgt=<file> --vocab=<file> [options]
     nmt.py decode [options] MODEL_PATH TEST_SOURCE_FILE OUTPUT_FILE
     nmt.py decode [options] MODEL_PATH TEST_SOURCE_FILE TEST_TARGET_FILE OUTPUT_FILE
     nmt.py pruneFunction [options] MODEL_PATH PRUNING_TYPE PERCENTAGE
@@ -613,6 +614,163 @@ def train(args: Dict):
                     exit(0)
     
 
+def retrain(args: Dict):
+    #appending <s> and </s> to all sentences
+    train_data_src = read_corpus(args['--train-src'], source='src')
+    train_data_tgt = read_corpus(args['--train-tgt'], source='tgt')
+
+    dev_data_src = read_corpus(args['--dev-src'], source='src')
+    dev_data_tgt = read_corpus(args['--dev-tgt'], source='tgt')
+    #preparing train data and dev data
+    train_data = list(zip(train_data_src, train_data_tgt))
+    dev_data = list(zip(dev_data_src, dev_data_tgt))
+    
+    train_batch_size = int(args['--batch-size'])
+    clip_grad = float(args['--clip-grad'])
+    valid_niter = int(args['--valid-niter'])
+    log_every = int(args['--log-every'])
+    model_save_path = args['--save-to']
+
+    vocab = Vocab.load(args['--vocab'])
+    #switch to training mode
+    model.train()
+
+    #doing uniform initialisation we need to try other initialisatin too
+    uniform_init = float(args['--uniform-init'])
+    if np.abs(uniform_init) > 0.:
+        print('uniformly initialize parameters [-%f, +%f]' % (uniform_init, uniform_init), file=sys.stderr)
+        for p in model.parameters():
+            p.data.uniform_(-uniform_init, uniform_init)
+
+    vocab_mask = torch.ones(len(vocab.tgt))
+    vocab_mask[vocab.tgt['<pad>']] = 0
+
+    device = torch.device("cuda:0" if args['--cuda'] else "cpu")
+    print('use device: %s' % device, file=sys.stderr)
+
+    model = model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=float(args['--lr']))
+
+    num_trial = 0
+    train_iter = patience = cum_loss = report_loss = cum_tgt_words = report_tgt_words = 0
+    cum_examples = report_examples = epoch = valid_num = 0
+    hist_valid_scores = []
+    train_time = begin_time = time.time()
+    print('begin Maximum Likelihood training')
+
+    while True:
+        epoch += 1
+
+        for src_sents, tgt_sents in batch_iter(train_data, batch_size=train_batch_size, shuffle=True):
+            train_iter += 1
+
+            optimizer.zero_grad()
+
+            batch_size = len(src_sents)
+
+            # (batch_size)
+            example_losses = -model(src_sents, tgt_sents)
+            batch_loss = example_losses.sum()
+            loss = batch_loss / batch_size
+
+            loss.backward()
+
+            # clip gradient to prevent exploding gradients
+            grad_norm = torch.nn.utils.clip_grad_norm(model.parameters(), clip_grad)
+
+            optimizer.step()
+
+            batch_losses_val = batch_loss.item()
+            report_loss += batch_losses_val
+            cum_loss += batch_losses_val
+
+            tgt_words_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting leading `<s>`
+            report_tgt_words += tgt_words_num_to_predict
+            cum_tgt_words += tgt_words_num_to_predict
+            report_examples += batch_size
+            cum_examples += batch_size
+
+            if train_iter % log_every == 0:
+                print('my iter %d'% (train_iter))
+                print('epoch %d, iter %d, avg. loss %.2f, avg. ppl %.2f ' \
+                      'cum. examples %d, speed %.2f words/sec, time elapsed %.2f sec' % (epoch, train_iter,
+                                                                                         report_loss / report_examples,
+                                                                                         math.exp(report_loss / report_tgt_words),
+                                                                                         cum_examples,
+                                                                                         report_tgt_words / (time.time() - train_time),
+                                                                                         time.time() - begin_time), file=sys.stderr)
+
+                train_time = time.time()
+                report_loss = report_tgt_words = report_examples = 0.
+
+            # perform validation
+            if train_iter % valid_niter == 0:
+                print('epoch %d, iter %d, cum. loss %.2f, cum. ppl %.2f cum. examples %d' % (epoch, train_iter,
+                                                                                         cum_loss / cum_examples,
+                                                                                         np.exp(cum_loss / cum_tgt_words),
+                                                                                         cum_examples), file=sys.stderr)
+
+                cum_loss = cum_examples = cum_tgt_words = 0.
+                valid_num += 1
+
+                print('begin validation ...', file=sys.stderr)
+
+                # compute dev. ppl and bleu
+                dev_ppl = evaluate_ppl(model, dev_data, batch_size=128)   # dev batch size can be a bit larger
+                valid_metric = -dev_ppl
+
+                print('validation: iter %d, dev. ppl %f' % (train_iter, dev_ppl), file=sys.stderr)
+                #we try out many models and take the best one intially ther is no model so first conditon is for that
+                is_better = len(hist_valid_scores) == 0 or valid_metric > max(hist_valid_scores)
+                hist_valid_scores.append(valid_metric)
+        
+                if is_better:
+                    patience = 0
+                    print('save currently the best model to [%s]' % model_save_path, file=sys.stderr)
+                    layers = get_layers(model)
+                    for i, j in layers:
+                        prune.remove(i,j[:-5])
+                    model.save(model_save_path)
+
+                    # also save the optimizers' state
+                    torch.save(optimizer.state_dict(), model_save_path + '.optim')
+                elif patience < int(args['--patience']):
+                    patience += 1
+                    print('hit patience %d' % patience, file=sys.stderr)
+
+                    if patience == int(args['--patience']):
+                        num_trial += 1
+                        print('hit #%d trial' % num_trial, file=sys.stderr)
+                        if num_trial == int(args['--max-num-trial']):
+                            print('early stop!', file=sys.stderr)#so if we get worse more than patience no of times we early stop it
+                            exit(0)
+
+                        # decay lr, and restore from previously best checkpoint
+                        lr = optimizer.param_groups[0]['lr'] * float(args['--lr-decay'])
+                        print('load previously best model and decay learning rate to %f' % lr, file=sys.stderr)
+
+                        # load model
+                        params = torch.load(model_save_path, map_location=lambda storage, loc: storage)
+                        model.load_state_dict(params['state_dict'])
+                        model = model.to(device)
+
+                        print('restore parameters of the optimizers', file=sys.stderr)
+                        optimizer.load_state_dict(torch.load(model_save_path + '.optim'))
+
+                        # set new lr
+                        for param_group in optimizer.param_groups:
+                            param_group['lr'] = lr
+
+                        # reset patience
+                        patience = 0
+
+                if epoch == int(args['--max-epoch']):
+                    print('reached maximum number of epochs!', file=sys.stderr)
+                    exit(0)
+    
+
+
 
 # In[ ]:
 
@@ -774,6 +932,10 @@ def main():
         decode(args)
     elif args['pruneFunction']:
         pruneFunction(args)
+    elif args['retrain']:
+        retrain(args)
+    elif args['pruneFunctionRetraining']:
+        pruneFunctionRetraining(args)
     else:
         raise RuntimeError(f'invalid run mode')
 
