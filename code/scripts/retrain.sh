ce03a2e494cc1921c08f9831551da35e2b454f93
Kyrylo Shyvam Kumar <kirilkumar@tutanota.com>
2023-11-03 21:36:51 +0530

    Now retraining is without init.

--- code/scripts/retrain.sh
+++ code/scripts/retrain.sh
@@ -27,14 +27,14 @@ python lstmModel.py \
     --batch-size 64 \
     --hidden-size 256 \
     --embed-size 256 \
-    --uniform-init 0.1 \
+    --uniform-init 0.0 \
     --label-smoothing 0.1 \
     --dropout 0.2 \
     --clip-grad 5.0 \
     --save-to ${work_dir}/model.bin \
     --lr-decay 0.5 \
     --max-epoch 10 \
-    --lr 0.005 \
+    --lr $4 \
     ${work_dir}/"$1" \
     "$2" \
     "$3" 
