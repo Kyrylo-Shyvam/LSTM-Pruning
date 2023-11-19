3a86ced88f3dacbdcc6b7af8495faf69155e88df
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-11-20 01:19:37 +0530

    added optimal brain damage

--- code/pruningMethods.py
+++ code/pruningMethods.py
@@ -97,6 +97,57 @@ class SNIP():
         print(self.scores)
         self.thresholding(model, percent)
         
-    
+class OBD():
+  def __init__(self):
+      pass
+
+  def get_mask(self, model):
+      layers = get_layers(model)
+      prune.global_unstructured(layers,pruning_method=prune.RandomUnstructured,amount=0.0)
+
+  def set_grad_back(self, model):
+
+      self.final_scores = []
+      returned_scores = []
+      for name, vector in model.named_parameters():
+          print(vector.shape,name)
+          if name == 'label_smoothing_loss.one_hot':
+              continue
+          self.final_scores.append(torch.clone(vector.grad).detach()**2 * vector**2 *(0.5))
+          returned_scores.append(self.final_scores[-1].flatten())
+          vector.grad.zero_()
+
+      return torch.cat(returned_scores)
+
+  def thresholding(self, model, percent):
+      percent = float(percent)
+      self.thresh = torch.topk(self.scores, int(self.scores.shape[0]*(percent)), largest=False, sorted=True)
+      thresh = self.thresh[0][-1]
+      print(thresh)
+      with torch.no_grad():
+        for score, (name, vector) in zip(self.final_scores, model.named_parameters()):
+            if name == 'label_smoothing_loss.one_hot':
+                continue
+            # print(vector,type(score))
+            vector[score <= thresh] = 0
+
+  def prune(self, model, data, batches, batch_size, percent, device):
+      batches=int(batches)
+      # batches = 1
+      self.get_mask(model)
+      percent=float(percent)
+      for idx, (data, target) in enumerate(batch_iter(data, batch_size=batch_size, shuffle=True)):
+
+          
+          loss= model(data, target).sum()
+          print(loss)
+          loss.backward()
+          print(idx)
+          if idx+1 == batches:
+              break
+      
+      self.scores = self.set_grad_back(model)
+      print(self.scores)
+      self.thresholding(model, percent)
             
 
